72e4c5006a8f4e2c4b52e573a6e3ad8394ede39c
AbdelazizKhater <abdelazizyoussef2002@gmail.com>
2023-05-10 15:07:11 +0200

    clean

--- tamagolem/src/it/unibs/fp/tamagolem/TextConstants.java
+++ tamagolem/src/it/unibs/fp/tamagolem/TextConstants.java
@@ -2,7 +2,7 @@ package it.unibs.fp.tamagolem;
 
 public class TextConstants {
 
-    public static final String FLUSH = "\033[H\033[2J";
+    public static final String FLUSH = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
     public static final String VINTO = "HA VINTO L'";
     public static final String VITA_TAMAGOLEM_DELL = "Vita Tamagolem dell' ";
     public static final String PIETRA_FORTE = "Pietra Forte";
@@ -24,13 +24,13 @@ public class TextConstants {
     public static final String PIETRA_ESAURITA = "Pietra esaurita, selezionarne un'altra";
     public static final String BENVENUTO = "BENVENUTO IN TAMAGOLEM!";
     public static final String SCELTA_DIFFICOLTA = "Scegli la difficoltà";
-    public static final String[] VOCI_MENU_PRINCIPALE = {"Facile", "Medio", "Difficile"};
-    public static final String ATTENZIONE_PAREGGIO = "ATTENZIONE: Le pietre selezionate causano un pareggio!";
+    public static final String[] VOCI_MENU_PRINCIPALE = { "Facile", "Medio", "Difficile" };
+    public static final String ATTENZIONE_PAREGGIO = "ATTENZIONE: Le pietre selezionate causano una situazione di stallo!";
     public static final String INSERIRE_NUOVAMENTE_PIETRE = "Inserire nuovamente le pietre del tamagolem o cambiare l'ordine di selezione";
     public static final String ELEMENTO_SCORTA = "- Elemento: ";
     public static final String PIETRE_SCORTA = "   Pietre: ";
     public static final String MESSAGGIO_EQUILIBRIO = "EQUILIBRIO DEL MONDO";
     public static final String FORMAT_EQUILIBRIO = "%10s";
-    public static final String NUOVA_PARTITA = "Vuoi giocare una nuova partita";    
+    public static final String NUOVA_PARTITA = "Vuoi giocare una nuova partita";
     public static final String ALLENATORE = "Allenatore ";
 }
